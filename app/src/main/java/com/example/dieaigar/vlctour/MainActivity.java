bb6d0c0a332f02b61fd2d02ad1d7f6b29de3bb4a
Diego Aineto <dieaigar@inf.upv.es>
2016-04-01 00:09:58 +0200

    Drawer con Fragments

--- app/src/main/java/com/example/dieaigar/vlctour/MainActivity.java
+++ app/src/main/java/com/example/dieaigar/vlctour/MainActivity.java
@@ -1,33 +1,40 @@
 package com.example.dieaigar.vlctour;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.dieaigar.vlctour.fragments.RoutesFragment;
+
+
+public class MainActivity extends ActionBarActivity {
 
     //First We Declare Titles And Icons For Our Navigation Drawer List View
     //This Icons And Titles Are holded in an Array as you can see
 
-    String TITLES[] = {"Home"};
-    int ICONS[] = {R.drawable.ic_home};
+    String TITLES[] = {"Home","Near me","What to see","Routes"};
+    int ICONS[] = {R.drawable.ic_home,R.drawable.ic_near,R.drawable.ic_pois,R.drawable.ic_routes};
 
     //Similarly we Create a String Resource for the name and email in the header view
     //And we also create a int resource for profile picture in the header view
 
-    String NAME = "Diego Aineto";
-    String EMAIL = "garciag1987@gmail.com";
+    String NAME = "Akash Bangad";
+    String EMAIL = "akash.bangad@android4devs.com";
     int PROFILE = R.drawable.avatar;
 
-    private Toolbar toolbar;
+    private Toolbar toolbar;                              // Declaring the Toolbar Object
 
     RecyclerView mRecyclerView;                           // Declaring RecyclerView
     RecyclerView.Adapter mAdapter;                        // Declaring Adapter For Recycler View
@@ -35,6 +42,9 @@ public class MainActivity extends AppCompatActivity {
     DrawerLayout Drawer;                                  // Declaring DrawerLayout
 
     ActionBarDrawerToggle mDrawerToggle;                  // Declaring Action Bar Drawer Toggle
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,12 +70,77 @@ public class MainActivity extends AppCompatActivity {
 
         mRecyclerView.setAdapter(mAdapter);                              // Setting the adapter to RecyclerView
 
+
+        final GestureDetector mGestureDetector = new GestureDetector(MainActivity.this, new GestureDetector.SimpleOnGestureListener() {
+
+            @Override public boolean onSingleTapUp(MotionEvent e) {
+                return true;
+            }
+
+        });
+
+
+        mRecyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
+            @Override
+            public boolean onInterceptTouchEvent(RecyclerView recyclerView, MotionEvent motionEvent) {
+                View child = recyclerView.findChildViewUnder(motionEvent.getX(), motionEvent.getY());
+
+
+                if (child != null && mGestureDetector.onTouchEvent(motionEvent)) {
+                    Drawer.closeDrawers();
+                    int position = recyclerView.getChildPosition(child);
+
+                    Fragment fragment = null;
+
+                    switch (position){
+                        case 1:
+                            Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                            break;
+                        case 2:
+                            Toast.makeText(MainActivity.this, "Near me", Toast.LENGTH_SHORT).show();
+                            break;
+                        case 3:
+                            Toast.makeText(MainActivity.this, "What to see", Toast.LENGTH_SHORT).show();
+                            break;
+                        case 4:
+                            Toast.makeText(MainActivity.this, "Routes", Toast.LENGTH_SHORT).show();
+                            fragment = new RoutesFragment();
+                            break;
+                    }
+
+                    if(fragment != null){
+                        // Insert the fragment by replacing any existing fragment
+                        FragmentManager fragmentManager = getFragmentManager();
+                        fragmentManager.beginTransaction()
+                                .replace(R.id.content_frame, fragment)
+                                .commit();
+                    }
+
+                    return true;
+
+                }
+
+                return false;
+            }
+
+            @Override
+            public void onTouchEvent(RecyclerView recyclerView, MotionEvent motionEvent) {
+
+            }
+
+            @Override
+            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+            }
+        });
+
+
         mLayoutManager = new LinearLayoutManager(this);                 // Creating a layout Manager
 
         mRecyclerView.setLayoutManager(mLayoutManager);                 // Setting the layout Manager
 
 
-        Drawer = (DrawerLayout) findViewById(R.id.DrawerLayout);        // Drawer object Assigned to the view
+        Drawer = (DrawerLayout) findViewById(R.id.drawer_layout);        // Drawer object Assigned to the view
         mDrawerToggle = new ActionBarDrawerToggle(this,Drawer,toolbar,R.string.openDrawer,R.string.closeDrawer){
 
             @Override
@@ -106,7 +181,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.menu_credits) {
-            Toast.makeText(MainActivity.this, getResources().getString(R.string.menu_credits), Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "Credits", Toast.LENGTH_SHORT).show();
             return true;
         }
 
---
+++ app/src/main/java/com/example/dieaigar/vlctour/fragments/RoutesFragment.java
@@ -0,0 +1,29 @@
+package com.example.dieaigar.vlctour.fragments;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.dieaigar.vlctour.R;
+
+import java.util.Locale;
+
+public class RoutesFragment extends Fragment {
+    public static final String ARG_PLANET_NUMBER = "planet_number";
+
+    public RoutesFragment() {
+        // Empty constructor required for fragment subclasses
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.routes_fragment, container, false);
+        getActivity().setTitle("Routes");
+        return rootView;
+    }
+}
