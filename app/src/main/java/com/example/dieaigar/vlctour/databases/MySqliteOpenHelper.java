a664ae6330a39918d00d979fa166cf451416660d
Manuel García Dolz <magardol@inf.upv.es>
2016-04-14 18:59:05 +0200

    added database + fix poi view (xml)

---
+++ app/src/main/java/com/example/dieaigar/vlctour/databases/MySqliteOpenHelper.java
@@ -0,0 +1,146 @@
+package com.example.dieaigar.vlctour.databases;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.example.dieaigar.vlctour.R;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MySqliteOpenHelper extends SQLiteOpenHelper {
+
+    //variables
+    private static MySqliteOpenHelper instance;
+    Context context;
+
+
+    public synchronized static MySqliteOpenHelper getInstance(Context context) {
+        if (instance == null) {
+            instance = new MySqliteOpenHelper(context.getApplicationContext());
+        }
+        return instance;
+    }
+
+    public MySqliteOpenHelper(Context context) {
+        super(context, "database_file", null, 1);
+        this.context=context;
+    }
+
+
+    //onCreate
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        try {
+            db.execSQL("CREATE TABLE pois (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT NOT NULL, tipo TEXT NOT NULL, coordenadas TEXT NOT NULL);");
+            parsecsv(db);
+        }catch(SQLException e){e.printStackTrace();}
+    }
+
+    private void parsecsv(SQLiteDatabase db){
+        InputStream csvFile = context.getResources().openRawResource(R.raw.pois);
+        String line = "";
+        String cvsSplitBy = ";";
+        BufferedReader br = null;
+        try {
+
+            br = new BufferedReader(new InputStreamReader(csvFile));
+            while ((line = br.readLine()) != null) {
+
+                String[] items = line.split(cvsSplitBy);
+                addPOI(items[0], items[1], items[2], db);
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    //onUpgrade, not really needed
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {  }
+
+
+    //get scores from database
+    public ArrayList<ArrayList<String>> getPOIs() {
+
+        ArrayList<ArrayList<String>> result = new ArrayList<>();
+        ArrayList<String> item;
+        SQLiteDatabase database = getReadableDatabase();
+        Cursor cursor = database.query("pois", new String[]{"nombre", "tipo", "coordenadas"}, null, null, null, null, null);
+
+        while (cursor.moveToNext()) {
+
+            item = new ArrayList<String>();
+            item.add(cursor.getString(0));
+            item.add(cursor.getString(1));
+            item.add(cursor.getString(2));
+            result.add(item);
+        }
+
+        cursor.close();
+        database.close();
+        return result;
+    }
+
+    //adding a score to the database
+    public void addPOI(String nombre, String tipo, String coordenadas) {
+
+        SQLiteDatabase database = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("nombre", nombre);
+        values.put("tipo", tipo);
+        values.put("coordenadas", coordenadas);
+        database.insert("pois", null, values);
+        database.close();
+    }
+
+    //adding a score to the database
+    public void addPOI(String nombre, String tipo, String coordenadas, SQLiteDatabase db) {
+
+        ContentValues values = new ContentValues();
+        values.put("nombre", nombre);
+        values.put("tipo", tipo);
+        values.put("coordenadas", coordenadas);
+        db.insert("pois", null, values);
+    }
+
+    //deletes everything in the database
+    public void deleteAllScores() {
+
+        SQLiteDatabase database = getWritableDatabase();
+        database.delete("pois", null, null);
+        database.close();
+    }
+}
--- app/src/main/java/com/example/dieaigar/vlctour/fragments/POIFragment.java
+++ app/src/main/java/com/example/dieaigar/vlctour/fragments/POIFragment.java
@@ -1,6 +1,5 @@
 package com.example.dieaigar.vlctour.fragments;
 
-
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -12,6 +11,9 @@ import android.view.ViewGroup;
 import com.example.dieaigar.vlctour.POI;
 import com.example.dieaigar.vlctour.POIAdapter;
 import com.example.dieaigar.vlctour.R;
+import com.example.dieaigar.vlctour.databases.MySqliteOpenHelper;
+
+import org.apache.commons.lang3.text.WordUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,15 +38,27 @@ Declarar instancias globales
         View rootView = inflater.inflate(R.layout.fragment_poi, container, false);
         getActivity().setTitle("Points Of Interest");
 
-
         // Inicializar POIs
         List<POI> items = new ArrayList<>();
+        MySqliteOpenHelper db =  MySqliteOpenHelper.getInstance(this.getActivity());
+        ArrayList<ArrayList<String>> pois = db.getPOIs();
+        String[] elements = new String[3];
 
+        for(int i=0;i<pois.size();i++){
+            elements[0]=elements[1]=elements[2]="";
+            for(int j = 0; j<elements.length; j++){
+                elements[j] = pois.get(i).get(j);
+            }
+            int poiimageid = getPOIImage(i);
+            items.add(new POI(poiimageid, WordUtils.capitalizeFully(elements[0], '\'', ' '), WordUtils.capitalizeFully(elements[1])));
+        }
+        /*
         items.add(new POI(R.drawable.poi_000, "Antiguo Hospital General", "Monument"));
         items.add(new POI(R.drawable.poi_001, "L'Almoina. Centro Arqueológico", "Monument"));
         items.add(new POI(R.drawable.poi_002, "Atarazanas", "Monument"));
         items.add(new POI(R.drawable.poi_003, "Museo Benlliure", "Museum"));
         items.add(new POI(R.drawable.poi_004, "Ermita de Santa Lucía", "Monument"));
+        */
 
         // Obtener el Recycler
         recycler = (RecyclerView) rootView.findViewById(R.id.reciclador);
@@ -58,5 +72,26 @@ Declarar instancias globales
         adapter = new POIAdapter(items);
         recycler.setAdapter(adapter);
         return rootView;
+    }
+
+    //get poi image id
+    private int getPOIImage(int index){
+        int poiimageid = 0;
+
+        switch(index){
+            case 0: poiimageid = R.drawable.poi_000;
+                break;
+            case 1: poiimageid = R.drawable.poi_001;
+                break;
+            case 2: poiimageid = R.drawable.poi_002;
+                break;
+            case 3: poiimageid = R.drawable.poi_003;
+                break;
+            case 4: poiimageid = R.drawable.poi_004;
+                break;
+            default: poiimageid = R.drawable.poi_000;
+        }
+        return poiimageid;
+
     }
 }
